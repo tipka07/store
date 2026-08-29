97f6f758503b6814950c51345592ca952075f4da
tipka07 <polvlass@gmail.com>
2026-08-29 20:03:37 +0500

    delete function

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ let myModal = new bootstrap.Modal(document.getElementById('myModal'),{
     keyboard: false
 })
 
+let options={
+    valueNames: ['name', 'price']
+}
+let userList
+
 document.querySelector('button.add-new').addEventListener('click', function(e) {
     let name = document.getElementById('good-name').value
     let price = document.getElementById('good-price').value
@@ -73,10 +78,40 @@ function update_goods(){
                 )
             }
         }
-        //userList = new List('goods', options);
+        userList = new List('goods', options);
     } else {
         table1.hidden = true
         table2.hidden = true
     }
     document.querySelector('.price-result').innerHTML = result_price + ' &#8381;'
 }
+
+document.querySelector('.list').addEventListener('click', function(e) {
+    if(!e.target.dataset.delete) {
+        return
+    } Swal.fire({
+        title: "Внимание!",
+        text: "Вы действительно хотите удалить товар?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Да',
+        cancelButtonText: 'Отмена',
+    }).then((result) => {
+        if(result.isConfirmed) {
+            let goods = JSON.parse(localStorage.getItem('goods'))
+            for(let i=0; i<goods.length; i++){
+                if(goods[i][0] == e.target.dataset.delete) {
+                    goods.splice(i, 1)
+                    localStorage.setItem('goods', JSON.stringify(goods))
+                    update_goods()
+                }
+            } Swal.fire(
+                "Удалено!",
+                "Выбранный товар был успешно удален.",
+                "success"
+            )
+        }
+    })
+})
